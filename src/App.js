33ad9f5a86cb4cd352d1d424365f7d8379793101
Bamlak11 <77420100+Bamlak11@users.noreply.github.com>
2024-05-07 15:35:31 -0500

    updated main branch

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import Announcement from './components/Announcement';
 import InputForm from './components/InputForm';
+import Announcement from './components/Announcement';
+import { useState } from 'react';
+
 
 function App() {
+  const [announcements, setAnnouncements] = useState([]);
+
+  const handleFormSubmit = (newAnnouncement) => {
+    setAnnouncements([newAnnouncement, ...announcements]);
+  };
+
   return (
     <>
-      <Announcement/>
-      <InputForm/>
+   {announcements.map((announcement, index) => (
+        <Announcement key={index} {...announcement} />
+      ))}
+
+      <InputForm onSubmit={handleFormSubmit}/>
+
     </>
   );
 }
--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const InputForm = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+const InputForm = ({ onSubmit }) => {
+    const [announcement, setAnnouncement] = useState({ message: '', name: '', date: '' });
+
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setAnnouncement({ ...announcement, [name]: value });
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      onSubmit(announcement);
+      setAnnouncement({ message: '', name: '', date: '' });
+    };
+  
+    return (
+      <form onSubmit={handleSubmit}>
+        <label>Message:</label>
+        <input type="text" name="message" placeholder="Message" value={announcement.message} onChange={handleChange} /><br></br>
+
+        <label>Name:</label>
+        <input type="text" name="name" placeholder="Name" value={announcement.name} onChange={handleChange} /><br></br>
+
+        <label>Date:</label>
+        <input type="date" name="date" value={announcement.date} onChange={handleChange} />
+       
+       <button type="submit">Submit</button>
+      </form>
+    );
+  };
 
 export default InputForm

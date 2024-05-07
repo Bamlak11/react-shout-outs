008f21a65a5937ce208007b51cfdf1ad236b2c90
Bamlak11 <77420100+Bamlak11@users.noreply.github.com>
2024-05-07 15:37:27 -0500

    branch 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import InputForm from './components/InputForm';
+import Announcement from './components/Announcement';
+import { useState } from 'react';
+
 
 function App() {
+  const [announcements, setAnnouncements] = useState([]);
+
+  const handleFormSubmit = (newAnnouncement) => {
+    setAnnouncements([newAnnouncement, ...announcements]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   {announcements.map((announcement, index) => (
+        <Announcement key={index} {...announcement} />
+      ))}
+
+      <InputForm onSubmit={handleFormSubmit}/>
+
+    </>
   );
 }
 
---
+++ src/components/Announcement.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Announcement = ({ message, name, date }) => {
+  return (
+    <div>
+      <h2>{message}</h2>
+      <p>Name: {name}</p>
+      <p>Date: {date}</p>
+    </div>
+  );
+};
+
+export default Announcement
